b0f3b85bd5eed6367b3378ebe07ee11480331e44
luizeduardofs <edu.dev.py@gmail.com>
2025-05-06 14:46:40 -0300

    Remove unsuali comments

--- electron/preload.ts
+++ electron/preload.ts
@@ -1,6 +1,5 @@
 import { contextBridge, ipcRenderer } from "electron";
 
-// --------- Expose some API to the Renderer process ---------
 contextBridge.exposeInMainWorld("ipcRenderer", {
   on(...args: Parameters<typeof ipcRenderer.on>) {
     const [channel, listener] = args;
